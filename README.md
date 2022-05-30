1f849471d2708eb3df652565fa9ab04a1b25c515
RaphaelHardFork <raphael.pellet@protonmail.com>
2022-05-30 15:35:31 +0200

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Template for EVM smart contracts
+
+_Using Foundry and Hardhat_
+
+## Installation
+
+```
+yarn
+forge update
+```
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,113 @@
-require("@nomiclabs/hardhat-waffle");
+require("@openzeppelin/hardhat-upgrades")
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-solhint")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-docgen")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("hardhat-preprocessor")
+require("dotenv").config()
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY
+const BSCSCAN_KEY = process.env.BSCSCAN_KEY
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="))
+}
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: "0.8.13",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+  networks: {
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    bscTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    bscMainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    polygonTestnet: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    polygonMainnet: {
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  docgen: {
+    path: "./docs",
+    clear: true,
+    runOnCompile: false,
+  },
+  gasReporter: {
+    currency: "EUR",
+    gasPrice: 21,
+    enabled: false,
+  },
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: false,
+    disambiguatePaths: false,
+  },
+  etherscan: {
+    apiKey: BSCSCAN_KEY,
+  },
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line) => {
+        if (line.match(/^\s*import /i)) {
+          getRemappings().forEach(([find, replace]) => {
+            if (line.match('"' + find)) {
+              line = line.replace('"' + find, '"' + replace)
+            }
+          })
+        }
+        return line
+      },
+    }),
+  },
+  paths: {
+    sources: "./src",
+    cache: "./cache_hardhat",
+  },
+}
